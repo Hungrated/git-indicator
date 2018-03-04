b67a06911e5242ca5b93324ab320c85e2b3f737c
hungrated <zhang295415658@qq.com>
2018-03-04 11:10:31 +0800

    dev: modify structure

--- git-indicator/Indicator/Indicator.swift
+++ git-indicator/Indicator/Indicator.swift
@@ -2,20 +2,17 @@
 //  Indicator.swift
 //  git-indicator
 //
-//  Created by Zihang Zhang on 2018/3/3.
-//  Copyright © 2018 Zihang Zhang. All rights reserved.
+//  Created by Zihang Zhang on 2018/3/4.
+//  Copyright © 2018年 Zihang Zhang. All rights reserved.
 //
 
 import Cocoa
 
-class Indicator: NSObject {
-    
-    @IBOutlet weak internal var popView: NSPopover!
-    
-    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-    
-    override func awakeFromNib() {
-        print("hello")
-    }
+class Indicator: NSViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
 }
--- git-indicator/MainController/MainController.swift
+++ git-indicator/MainController/MainController.swift
@@ -2,7 +2,7 @@
 //  MainController.swift
 //  git-indicator
 //
-//  Created by Zihang Zhang on 2018/3/3.
+//  Created by Zihang Zhang on 2018/3/4.
 //  Copyright © 2018 Zihang Zhang. All rights reserved.
 //
 
@@ -10,18 +10,47 @@ import Cocoa
 
 class MainController: NSObject {
     
-    @IBOutlet weak var menu: NSMenu!
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    let menu = NSMenu()
+    let popover = NSPopover()
     
     override func awakeFromNib() {
-        let icon = NSImage(named: NSImage.Name(rawValue: "StatusIcon"))
-        icon?.isTemplate = true
-        statusItem.title = "Git"
-        statusItem.image = icon
+        controllerInit()
+    }
+    
+    func controllerInit() {
+        if let button = statusItem.button {
+            button.image = NSImage(named: NSImage.Name(rawValue: "StatusIcon"))
+            button.action = #selector(MainController.togglePopover(sender:))
+        }
+        
+        popover.contentViewController = Indicator(nibName: NSNib.Name(rawValue: "Indicator"), bundle: nil)
+        
+        menu.addItem(NSMenuItem(title: "退出", action: #selector(MainController.quitClicked(sender:)), keyEquivalent: "q"))
+        
         statusItem.menu = menu
     }
     
-    @IBAction func quitClicked(sender: NSMenuItem) {
+    @objc func quitClicked(sender: AnyObject?) {
         NSApplication.shared.terminate(self)
     }
+    
+    @objc func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+    
+    @objc func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
+    @objc func togglePopover(sender: AnyObject?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
 }
+
