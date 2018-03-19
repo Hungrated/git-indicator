73570ed4d680fdc4ca66d6dd8e055419620cd252
hungrated <zhang295415658@qq.com>
2018-03-19 15:20:26 +0800

    feat: modify styles

--- git-indicator/Indicator/mainView/public/index.js
+++ git-indicator/Indicator/mainView/public/index.js
@@ -1,12 +1,12 @@
 $(function () {
-    // $.ajax({
-    //     method: 'get',
-    //     dataType: 'jsonp',
-    //     url: 'https://github.com/Hungrated',
-    //     success: 'handleData'
-    // })
-    //
-    // function handleData(data) {
-    //     console.log(data)
-    // }
+    $.ajax({
+        method: 'get',
+        dataType: 'jsonp',
+        url: 'https://api.github.com/users/Hungrated',
+        success: 'handleData'
+    })
+
+    function handleData(data) {
+        console.log(data)
+    }
 })
