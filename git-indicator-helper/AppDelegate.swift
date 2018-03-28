725e5e4edcba31c4f1a7ba08a1897eabf0184ef6
hungrated <zhang295415658@qq.com>
2018-03-28 13:30:17 +0800

    feat: add app helper

---
+++ git-indicator-helper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  git-indicator-helper
+//
+//  Created by Zihang Zhang on 2018/3/28.
+//  Copyright © 2018年 Zihang Zhang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
